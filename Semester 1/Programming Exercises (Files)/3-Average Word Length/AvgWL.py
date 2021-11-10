2191b4cf66e7b6ea4c129986b13fa83895bf09a8
Gitroars <501sterror@gmail.com>
2021-11-10 15:10:52 +0700

    Update AvgWL.py
    Finished with comments

--- Semester 1/Programming Exercises (Files)/3-Average Word Length/AvgWL.py
+++ Semester 1/Programming Exercises (Files)/3-Average Word Length/AvgWL.py
@@ -1,2 +1,20 @@
+#Numbers Initial Value
+averageWordLength = 0
+sumOfLengthOfWordTokens = 0
+numberOfWordTokens = 0
+
 bookFile = open("book.txt", "r")
 content = bookFile.read()
+splitWords=content.split() #Creates a list composed of splitted words as the items
+
+for x in splitWords: #For every item in the list,
+    numberOfWordTokens +=1 #Adds the variable value by 1
+    sumOfLengthOfWordTokens += len(x) #Adds the variable value by the length of the item
+
+#Calculates the average word length by dividing the sum of all the lengths of word tokens with number of word tokens
+averageWordLength=sumOfLengthOfWordTokens/numberOfWordTokens
+
+#Prints the average word length along with other number values
+print("Average Word Length: ",averageWordLength )
+print("\nSum of All Lengths of Word Tokens: ",sumOfLengthOfWordTokens)
+print("Number of Word Tokens: ",numberOfWordTokens)
