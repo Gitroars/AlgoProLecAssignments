2abc7ccaf9e715415e92ad704541ff9894ecf17c
Gitroars <501sterror@gmail.com>
2021-11-10 14:26:19 +0700

    Update nnl.py
    Added Comments

--- Semester 1/Programming Exercises (Files)/2- New Numbered Line/nnl.py
+++ Semester 1/Programming Exercises (Files)/2- New Numbered Line/nnl.py
@@ -1,11 +1,10 @@
 oldFile = open("oldFile.txt","r")
-oldContent = oldFile.readlines()
-print(oldContent)
+oldContent = oldFile.readlines() #Each line of oldFile becomes a member of the list
 newFile = open("newFile.txt","w")
-i = 1
-for line in oldContent:
- newFile.write(f"{i} "+line)
- i+=1
+i = 1 #numbering starts at number 1
+for line in oldContent: #At every list(line),
+ newFile.write(f"{i} "+line) #Write the value of i(for numbering) before the line sentence
+ i+=1 #Afterwards, increase the value of i by 1
 
 
 
