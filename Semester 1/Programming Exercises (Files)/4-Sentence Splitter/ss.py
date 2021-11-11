0c771ff703e0dbab56a1354c8897aba2fed8dc37
Gitroars <501sterror@gmail.com>
2021-11-11 19:04:41 +0700

    no4 progress
    major progression

--- Semester 1/Programming Exercises (Files)/4-Sentence Splitter/ss.py
+++ Semester 1/Programming Exercises (Files)/4-Sentence Splitter/ss.py
@@ -1,10 +1,35 @@
+shortTitles = ["Mr.", "Mrs.", "Dr."]
+longTitles = ["Mister", "Missus", "Doctor"]
+shortInternal = ["e.g.", "i.e."]
+longInternal = ["EG", "IE"]
 
-defaultInput="Mr. Miyagi bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't."
-# fileContent=input("Enter the file's content: ")
 
-defaultInput=defaultInput.replace(". ",".\n ")
-defaultInput=defaultInput.replace("?","?\n ")
-defaultInput=defaultInput.replace("!","!\n ")
-file=open("result.txt","w")
+
+defaultInput = "Mr. Miyagi bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't."
+
+
+
+
+for x in shortTitles:
+     i = shortTitles.index(x)
+     defaultInput = defaultInput.replace(x, longTitles[i])
+for x in shortInternal:
+     i = shortInternal.index(x)
+     defaultInput = defaultInput.replace(x,longInternal[i])
+
+defaultInput = defaultInput.replace(". ", ".\n ")
+defaultInput = defaultInput.replace("?", "?\n ")
+defaultInput = defaultInput.replace("!", "!\n ")
+
+for x in longTitles:
+    i = longTitles.index(x)
+    defaultInput = defaultInput.replace(x, shortTitles[i])
+for x in longInternal:
+    i = longInternal.index(x)
+    defaultInput = defaultInput.replace(x, shortInternal[i])
+
+file = open("result.txt", "w")
 file.write(defaultInput)
 file.close()
+
+
