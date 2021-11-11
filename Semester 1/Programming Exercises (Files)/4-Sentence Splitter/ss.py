a29cf18fab929f85d6f74a2c426e54166e61c9e6
Gitroars <501sterror@gmail.com>
2021-11-11 19:27:39 +0700

    No4
    Somewhat done

--- Semester 1/Programming Exercises (Files)/4-Sentence Splitter/ss.py
+++ Semester 1/Programming Exercises (Files)/4-Sentence Splitter/ss.py
@@ -1,35 +1,31 @@
-shortTitles = ["Mr.", "Mrs.", "Dr."]
-longTitles = ["Mister", "Missus", "Doctor"]
+#A pair of list to convert special exceptions from and to back and forth
+shortTitles = ["Mr.", "Mrs.", "Dr.","Jr."]
+longTitles = ["Mister", "Missus", "Doctor","JR"]
 shortInternal = ["e.g.", "i.e."]
 longInternal = ["EG", "IE"]
 
+shortText=input("Enter a short text: ") #Asks the user to input the short text
 
-
-defaultInput = "Mr. Miyagi bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it. Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true... Well, with a probability of .9 it isn't."
-
-
-
-
-for x in shortTitles:
+for x in shortTitles: #Replaces titles with another name to prevent periods replacement
      i = shortTitles.index(x)
-     defaultInput = defaultInput.replace(x, longTitles[i])
-for x in shortInternal:
+     shortText = shortText.replace(x, longTitles[i])
+for x in shortInternal: #Replaces periods internal with another name to prevent periods replacement
      i = shortInternal.index(x)
-     defaultInput = defaultInput.replace(x,longInternal[i])
+     shortText = shortText.replace(x, longInternal[i])
 
-defaultInput = defaultInput.replace(". ", ".\n")
-defaultInput = defaultInput.replace("?", "?\n")
-defaultInput = defaultInput.replace("!", "!\n")
+shortText = shortText.replace(". ", ".\n") #Replaces period with a period and new line
+shortText = shortText.replace("? ", "?\n") #Replaces question mark with a question mark and new line
+shortText = shortText.replace("! ", "!\n")  #Replaces exclamation mark with an exclamation mark and new line
 
-for x in longTitles:
+for x in longTitles: #Reverts  new title name back to old name
     i = longTitles.index(x)
-    defaultInput = defaultInput.replace(x, shortTitles[i])
-for x in longInternal:
+    shortText = shortText.replace(x, shortTitles[i])
+for x in longInternal: #Reverts new internal name back to old name
     i = longInternal.index(x)
-    defaultInput = defaultInput.replace(x, shortInternal[i])
+    shortText = shortText.replace(x, shortInternal[i])
 
 file = open("result.txt", "w")
-file.write(defaultInput)
+file.write(shortText) #Write the modified short text
 file.close()
 
 
